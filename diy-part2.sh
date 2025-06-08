6c63ea98e53567c71b19a01b4a5c36cdb40f6df0
Link Zhang <Link.Zhang.0@gmail.com>
2025-06-08 14:27:37 +0800

    v0.2

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,11 +16,14 @@
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-sed -i 's/LEDE/Pi5Plus/g' package/base-files/files/bin/config_generate
+# Modify hostname  failure
+#sed -i 's/LEDE/Pi5Plus/g' package/base-files/files/bin/config_generate
 
 # Modify Luci Login Page
 sed -i '23,25d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+
+# Modify Luci Page
+sed -i '23,26d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 
 # Change ash to bash
 sed -i 's|/bin/ash$|/bin/bash|g' package/base-files/files/etc/passwd
