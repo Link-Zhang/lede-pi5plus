da396e7bfcca5c249da63e54501ed45c4e4dc1b1
Link Zhang <Link.Zhang.0@gmail.com>
2025-06-19 17:11:44 +0800

    v0.0.5: add ax210 support and modify files

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,15 @@
 # See /LICENSE for more information.
 #
 
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/luci2/bin/config_generate
+
+# Modify hostname
+sed -i 's/LEDE/Pi5Plus/g' package/base-files/luci2/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+
 # Modify Luci Login Page
 sed -i '23,25d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
 
