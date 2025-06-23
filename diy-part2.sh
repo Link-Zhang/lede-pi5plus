25c6f9f3c1e0e3b5db4425d480344074b86a5089
Link Zhang <Link.Zhang.0@gmail.com>
2025-06-23 13:33:46 +0800

    v0.1.1: use kernel 6.6 and wpad

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,7 +30,7 @@ sed -i 's|/bin/ash$|/bin/bash|g' package/base-files/files/etc/passwd
 
 # Change kernel to 6.6
 sed -i 's|6.12|6.6|g' target/linux/rockchip/Makefile
-sed -i 's|6.6|6.6|g' target/linux/rockchip/Makefile
+#sed -i 's|6.6|6.6|g' target/linux/rockchip/Makefile
 
 # Change ttyd from services to system
 sed -i 's|/services/|/system/|g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
